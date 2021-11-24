313f4d6944a663230e22388409ca17615a119e6c
Julia Prein <jprein@me.com>
2021-11-24 16:35:42 +0100

    adjust index page, translate

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ mdc.ripple.MDCRipple.attachTo(document.getElementById('button-center-item'));
 const button = document.getElementById('button-center-item');
 let continueIDOK = false;
 let continueVOK = false;
+let continueLOK = false;
 
 // FOR INPUT FORM
 const textField = document.querySelector('.mdc-text-field__input');
@@ -12,23 +13,25 @@ const handleInput = (event) => {
   // to get most recent value, get element fresh
   // count number of characters and display the count
   document.querySelector('.mdc-text-field-character-counter').innerHTML = 
-    `${document.querySelector('.mdc-text-field__input').value.length} / 8`;
+    `${document.querySelector('.mdc-text-field__input').value.length} / 10`;
 
-  continueIDOK = (document.querySelector('.mdc-text-field__input').value.length === 8);
-  // enable button when eight characters are entered
-  button.disabled = !(continueIDOK && continueVOK);
+  // FOR DEMO, ALLOW ANY LENGTH OF ID
+  continueIDOK = (document.querySelector('.mdc-text-field__input').value.length > 0);
+
+  // enable button when at least one character is entered
+  button.disabled = !(continueIDOK && continueVOK && continueLOK);
 };
 
 textField.addEventListener('keyup', handleInput, { capture: false });
 
 // FOR STUDY VERSION
 // get both radio buttons
-const radios = document.getElementsByName('radios');
+const radiosV = document.getElementsByName('radios-v');
 let studyversion = null;
 
 // when users click on a button...
-for (radio in radios) {
-  radios[radio].onclick = function() {
+for (radio in radiosV) {
+  radiosV[radio].onclick = function() {
     
     // if first 'yes' button is selected, save consent as true
     if (this.getAttribute('id') == 'radio-box') {
@@ -46,7 +49,36 @@ for (radio in radios) {
     continueVOK = true;
     
     // enable submit button
-    button.disabled = !(continueIDOK && continueVOK);
+    button.disabled = !(continueIDOK && continueVOK && continueLOK);
+  }
+}
+
+// FOR LANGUAGE
+// get both radio buttons
+const radiosLang = document.getElementsByName('radios-lang');
+let lang = null;
+
+// when users click on a button...
+for (radio in radiosLang) {
+  radiosLang[radio].onclick = function() {
+    
+    // if first 'yes' button is selected, save consent as true
+    if (this.getAttribute('id') == 'radio-en') {
+      lang = 'en';
+    
+    // if second 'no' button is selected, save consent as false
+    } else if (this.getAttribute('id') == 'radio-de') {
+      lang = 'de';
+
+    // should never reach here, just for security
+    } else {
+      lang = 'en';
+    }
+
+    continueLOK = true;
+    
+    // enable submit button
+    button.disabled = !(continueIDOK && continueVOK && continueLOK);
   }
 }
 
@@ -57,7 +89,7 @@ const handleContinueClick = (event) => {
   const subjID = document.querySelector('.mdc-text-field__input').value;
 
   // inhouse variable hardcoded to true for kiga version
-  window.location.href = `https://ccp-odc.eva.mpg.de/gafo-demo/experiment.html?inhouse=true&ID=${subjID}&v=${studyversion}`;
+  console.log(window.location.href = `https://ccp-odc.eva.mpg.de/gafo-demo/experiment.html?ID=${subjID}&v=${studyversion}&lang=${lang}`);
 };
 
 button.addEventListener('click', handleContinueClick, { capture: false });
